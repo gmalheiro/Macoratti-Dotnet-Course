59015a92abc4f109f3d6951e3bfb36dadecd2d1c
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-08 22:30:00 -0300

    Defined tables relationship in Categoria and Produto class

--- ApiCatalogo/ApiCatalogo/Models/Produto.cs
+++ ApiCatalogo/ApiCatalogo/Models/Produto.cs
@@ -8,5 +8,7 @@
         public string? ImagemUrl { get; set; }
         public float Estoque { get; set; }
         public DateTime DataCadastro { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria? Categoria { get; set; }
     }
 }
