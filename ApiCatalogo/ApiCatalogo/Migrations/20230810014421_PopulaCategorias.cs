1be14fedc03c44ff40190c411cf08bfde4137d95
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-09 22:47:42 -0300

    Added content for Up method

--- ApiCatalogo/ApiCatalogo/Migrations/20230810014421_PopulaCategorias.cs
+++ ApiCatalogo/ApiCatalogo/Migrations/20230810014421_PopulaCategorias.cs
@@ -8,7 +8,9 @@ namespace ApiCatalogo.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql("INSERT INTO categorias(Nome,ImagemUrl) values ('Bebidas','bebidas.jpg')");
+            migrationBuilder.Sql("INSERT INTO categorias(Nome,ImagemUrl) values ('Lanches','lanches.jpg')");
+            migrationBuilder.Sql("INSERT INTO categorias(Nome,ImagemUrl) values ('Sobremesas','sobremesas.jpg')");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
