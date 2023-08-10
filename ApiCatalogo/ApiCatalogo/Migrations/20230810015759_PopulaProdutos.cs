77d7bb9f9e0775aa469159b1f832d9a56f7e2864
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-09 22:58:18 -0300

    Added Migrations to populate Produtos table

---
+++ ApiCatalogo/ApiCatalogo/Migrations/20230810015759_PopulaProdutos.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiCatalogo.Migrations
+{
+    public partial class PopulaProdutos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
