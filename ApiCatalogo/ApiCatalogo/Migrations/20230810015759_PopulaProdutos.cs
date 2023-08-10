d6c02b413804597c9fef21207c4b8372abf642b0
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-09 23:04:16 -0300

    Added content to populate table produtos in up method

--- ApiCatalogo/ApiCatalogo/Migrations/20230810015759_PopulaProdutos.cs
+++ ApiCatalogo/ApiCatalogo/Migrations/20230810015759_PopulaProdutos.cs
@@ -8,7 +8,14 @@ namespace ApiCatalogo.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.Sql("INSERT INTO produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" + 
+                                 "VALUES ('Coca-Cola Diet','Refrigerante de Cola 350 ml', 5.45,'cocacola.jpg',50,now(),1 )");
 
+            migrationBuilder.Sql("INSERT INTO produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" +
+                                "VALUES ('Lanche de Atum','Lanche de Atum com maionese', 8.50,'atum.jpg',10,now(),2 )");
+
+            migrationBuilder.Sql("INSERT INTO produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" +
+                                "VALUES ('Pudim','Pudim de leite condensado 100g', 6.75,'pudim.jpg',20,now(),3)");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
