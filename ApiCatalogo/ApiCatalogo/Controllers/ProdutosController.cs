9c52c3087fef32192c619ade1ed700ca0868e3ad
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-10 18:28:29 -0300

    Created ProdutosController

---
+++ ApiCatalogo/ApiCatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,21 @@
+﻿using ApiCatalogo.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCatalogo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext? _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+    }
+}
