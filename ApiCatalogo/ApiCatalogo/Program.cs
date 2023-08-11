8b850f4e987679b1244855ed7183067117bb6dca
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-11 17:45:58 -0300

    Added json serializer

--- ApiCatalogo/ApiCatalogo/Program.cs
+++ ApiCatalogo/ApiCatalogo/Program.cs
@@ -1,5 +1,6 @@
 using ApiCatalogo.Context;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 
 string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
 
