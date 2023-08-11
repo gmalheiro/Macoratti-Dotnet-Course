09dc15537559c1ae8c36efb0f7c31fa3e5cbe5c3
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-11 18:02:08 -0300

    Added JsonIgnore to prop categoria in Produto class

--- ApiCatalogo/ApiCatalogo/Models/Produto.cs
+++ ApiCatalogo/ApiCatalogo/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace ApiCatalogo.Models
 {
@@ -27,6 +28,7 @@ namespace ApiCatalogo.Models
         public float Estoque { get; set; }
         public DateTime DataCadastro { get; set; }
         public int CategoriaId { get; set; }
+        [JsonIgnore]
         public Categoria? Categoria { get; set; }
     }
 }
