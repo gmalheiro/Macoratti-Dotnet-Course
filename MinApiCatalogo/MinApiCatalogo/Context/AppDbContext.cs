15d997c8c5b3185163b060850870d970ba76a89e
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-28 03:03:23 -0300

    Created AppDbContext class

---
+++ MinApiCatalogo/MinApiCatalogo/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinApiCatalogo.Models;
+
+namespace MinApiCatalogo.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options){}
+        public DbSet<Categoria>? Categoria { get; set; }
+        public DbSet<Produto>? Produto { get; set; }
+    }
+}
