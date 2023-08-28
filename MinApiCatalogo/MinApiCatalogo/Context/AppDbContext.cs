ade8d87447c97fa6328fc94b182d10ade67b81a1
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-28 03:27:46 -0300

    Applied OnModelCreating method

--- MinApiCatalogo/MinApiCatalogo/Context/AppDbContext.cs
+++ MinApiCatalogo/MinApiCatalogo/Context/AppDbContext.cs
@@ -8,5 +8,28 @@ namespace MinApiCatalogo.Context
         public AppDbContext(DbContextOptions<AppDbContext> options){}
         public DbSet<Categoria>? Categoria { get; set; }
         public DbSet<Produto>? Produto { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            //Categoria
+            mb.Entity<Categoria>().HasKey(c => c.CategoriaId);
+            mb.Entity<Categoria>().Property(c => c.Nome)
+                                  .HasMaxLength(100)
+                                   .IsRequired();
+            mb.Entity<Categoria>().Property(c => c.Descricao).HasMaxLength(150).IsRequired();
+
+            //Produto
+            mb.Entity<Produto>().HasKey(p => p.ProdutoId );
+            mb.Entity<Produto>().Property(p => p.Nome).HasMaxLength(100).IsRequired();
+            mb.Entity<Produto>().Property(p => p.Descricao).HasMaxLength(150).IsRequired();
+            mb.Entity<Produto>().Property(p => p.Imagem).HasMaxLength(100).IsRequired();
+            mb.Entity<Produto>().Property(p => p.Preco).HasPrecision(14,2);
+
+            //relacionamento
+            mb.Entity<Produto>()
+                .HasOne<Categoria>(c => c.Categoria)
+                    .WithMany(p => p.Produtos)
+                    .HasForeignKey(c => c.CategoriaId);
+        }
     }
 }
