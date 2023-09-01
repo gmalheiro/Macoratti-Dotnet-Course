2511c8fd95240c2503967232ae4989499e6f50ef
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-31 22:41:46 -0300

    Applied JsonIgnore to Categoria class in Produtos field

--- MinApiCatalogo/MinApiCatalogo/Models/Categoria.cs
+++ MinApiCatalogo/MinApiCatalogo/Models/Categoria.cs
@@ -1,10 +1,13 @@
-﻿namespace MinApiCatalogo.Models
+﻿using System.Text.Json.Serialization;
+
+namespace MinApiCatalogo.Models
 {
     public class Categoria
     {
         public int CategoriaId { get; set; }
         public string? Nome { get; set; }
         public string? Descricao { get; set; }
+        [JsonIgnore]
         public ICollection<Produto>? Produtos { get; set; }
     }
 }
