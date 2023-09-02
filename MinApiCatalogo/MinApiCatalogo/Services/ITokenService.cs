df47d6e15602771d9614edff558a5d41c6b0cecb
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-09-02 19:34:36 -0300

    Created interface ITokenService

---
+++ MinApiCatalogo/MinApiCatalogo/Services/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using MinApiCatalogo.Models;
+
+namespace MinApiCatalogo.Services
+{
+    public interface ITokenService
+    {
+        string GerarToken(string key, string issuer, string audience,UserModel user);
+    }
+}
