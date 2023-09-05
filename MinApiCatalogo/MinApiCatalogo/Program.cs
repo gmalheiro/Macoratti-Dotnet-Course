18cce47211160e88c42f7af640dc002c5487eaed
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-09-05 20:20:16 -0300

    Added ProdutosEndpoints method

--- MinApiCatalogo/MinApiCatalogo/Program.cs
+++ MinApiCatalogo/MinApiCatalogo/Program.cs
@@ -79,72 +79,10 @@ if (app.Environment.IsDevelopment())
 }
 
 
-//endpoint para login
 app.MapAutenticacaoEndpoints();
-
-//Enpoint para Hello World
 app.MapGet("/", () => "Catálogo de produtos - 2023").ExcludeFromDescription();
-
-// Endpoins para categorias
 app.MapCategoriasEndpoints();
-
-//-------------------------endpoints para produtos---------------------------------
-app.MapPost("/produtos", async (AppDbContext db, Produto produto) =>
-{
-    if (produto is null)
-        return Results.BadRequest("Produto nulo");
-
-    db.Produto?.AddAsync(produto);
-
-    await db?.SaveChangesAsync()!;
-
-    return Results.Created($"/produto/{produto.ProdutoId}", produto);
-});
-
-app.MapGet("/produtos", async (AppDbContext db) => await db?.Produto?.ToListAsync()!).WithTags("Produtos").RequireAuthorization();
-
-app.MapGet("/produtos/{id:int}", async (AppDbContext db, int id) =>
-{
-    return await db.FindAsync<Produto>(id)
-           is Produto produto
-           ? Results.Ok(produto)
-           : Results.NotFound("Produto não encontrado");
-});
-
-app.MapPut("/produtos/{id:int}", async (int id, AppDbContext db, Produto produto) =>
-{
-    if (id != produto.ProdutoId)
-        return Results.BadRequest();
-
-    var produtoDb = await db.Produto!.FindAsync(id);
-    if (produtoDb is null)
-        return Results.NotFound("Produto não encontrada");
-
-    produtoDb.Nome = produto.Nome;
-    produtoDb.Descricao = produto.Descricao;
-    produtoDb.Preco = produto.Preco;
-    produtoDb.Imagem = produto.Imagem;
-    produtoDb.DataCompra = produto.DataCompra;
-    produtoDb.Estoque = produto.Estoque;
-    produtoDb.CategoriaId = produto.CategoriaId;
-
-    await db.SaveChangesAsync();
-
-    return Results.Ok(produtoDb);
-});
-
-app.MapDelete("/produtos/{id:int}", async (AppDbContext db, int id) =>
-{
-    var produto = await db.Produto!.FindAsync(id);
-
-    if (produto is null)
-
-        db.Produto.Remove(produto!);
-
-    await db.SaveChangesAsync();
-
-    return Results.Ok(produto);
-});
+app.MapProdutosEndpoints();
 
 app.UseHttpsRedirection();
 
