2e6324eb67f957854d950295d60c4be2e2fe2474
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-09-05 20:28:39 -0300

    Created static method for ExceptionHandling

---
+++ MinApiCatalogo/MinApiCatalogo/AppServiceExtensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace MinApiCatalogo.AppServiceExtensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static  IApplicationBuilder UseExceptionHandling(this IApplicationBuilder app,
+            IWebHostEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            return app;
+        }
+    }
+}
