0a691a971709868d8bb89ed7b450ae0ba7afb5a9
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-09-05 20:14:15 -0300

    Moved authentication endpoint to AutenticacaoEndpointClass

---
+++ MinApiCatalogo/MinApiCatalogo/ApiEndpoints/AutenticacaoEndpoints.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using MinApiCatalogo.Models;
+using MinApiCatalogo.Services;
+
+namespace MinApiCatalogo.ApiEndpoints;
+public static class AutenticacaoEndpoints
+{
+    public static void MapAutenticacaoEndpoints(this WebApplication app)
+    {
+        app.MapPost("/login", [AllowAnonymous] (UserModel userModel, ITokenService tokenService) =>
+        {
+            if (userModel == null)
+            {
+                return Results.BadRequest("Login Inválido");
+            }
+            if (userModel.UserName == "DotnetMalheiro" && userModel.Password == "numsey#123")
+            {
+                var tokenString = tokenService.GerarToken(app?.Configuration["Jwt:Key"]!,
+                    app?.Configuration["Jwt:Issuer"]!,
+                    app?.Configuration["Jwt:Audience"]!,
+                    userModel);
+                return Results.Ok(new { token = tokenString });
+            }
+            else
+            {
+                return Results.BadRequest("Login Inv�lido");
+            }
+        }).Produces(StatusCodes.Status400BadRequest)
+          .Produces(StatusCodes.Status200OK)
+          .WithName("Login")
+          .WithTags("Autenticacao");
+    }
+}
+
