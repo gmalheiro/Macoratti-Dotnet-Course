872c5a9f82f6a72e126b334ea69bfdcee1ba1796
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-09-13 21:16:37 -0300

    Implemented in Produto class

--- ApiCatalogo - Fundamentos/ApiCatalogo/Models/Produto.cs
+++ ApiCatalogo - Fundamentos/ApiCatalogo/Models/Produto.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using ApiCatalogo.Validations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
@@ -12,6 +13,7 @@ namespace ApiCatalogo.Models
 
         [Required(ErrorMessage = "O nome é obrigatório")]
         [StringLength(20,ErrorMessage ="O nome deve ter entre 5 e 20 caracteres",MinimumLength = 5)]
+        [PrimeiraLetraMaiuscula]
         public string? Nome { get; set; }
 
         [Required]
