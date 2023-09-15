6fbae0d888887f26cc6550309b5244a6d7b927cf
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-09-14 21:10:35 -0300

    ApiLoggingFilter class done

--- ApiCatalogo - Fundamentos/ApiCatalogo/Filters/ApiLoggingFilter.cs
+++ ApiCatalogo - Fundamentos/ApiCatalogo/Filters/ApiLoggingFilter.cs
@@ -1,6 +1,35 @@
-﻿namespace ApiCatalogo.Filters
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ApiCatalogo.Filters
 {
-    public class ApiLoggingFilter
+    public class ApiLoggingFilter : IActionFilter
     {
+
+        private readonly ILogger<ApiLoggingFilter> _logger;   
+
+        public ApiLoggingFilter(ILogger<ApiLoggingFilter> logger)
+        {
+                _logger = logger;
+        }
+
+        // Executa antes da action
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            _logger.LogInformation("### Executando -> OnActionExecuting");
+            _logger.LogInformation("###################################################");
+            _logger.LogInformation($" {DateTime.Now.ToLongTimeString()}");
+            _logger.LogInformation($"ModelState ({context.ModelState.IsValid}");
+            _logger.LogInformation("###################################################");
+        }
+        //Executa depois da action
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            _logger.LogInformation("### Executando -> OnActionExecuting");
+            _logger.LogInformation("###################################################");
+            _logger.LogInformation($" {DateTime.Now.ToLongTimeString()}");
+            _logger.LogInformation($"ModelState ({context.ModelState.IsValid}");
+            _logger.LogInformation("###################################################");
+        }
+
     }
 }
