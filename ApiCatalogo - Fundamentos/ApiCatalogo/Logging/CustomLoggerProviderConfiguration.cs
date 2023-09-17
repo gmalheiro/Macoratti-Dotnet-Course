dda173f5188d55bf779c2cbcbfe59176c3d43e32
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-09-17 13:20:52 -0300

    CustomLoggerProviderConfiguration done

--- ApiCatalogo - Fundamentos/ApiCatalogo/Logging/CustomLoggerProviderConfiguration.cs
+++ ApiCatalogo - Fundamentos/ApiCatalogo/Logging/CustomLoggerProviderConfiguration.cs
@@ -2,5 +2,7 @@
 {
     public class CustomLoggerProviderConfiguration
     {
+        public LogLevel LogLevel { get; set; } = LogLevel.Warning;
+        public int EventId { get; set; } = 0;
     }
 }
