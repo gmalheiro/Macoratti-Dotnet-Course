9b854c9739bf1329f7b72cb6f5dab814a3a0d5e4
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-09-17 13:37:56 -0300

    CustomLoggerProvider class done

--- ApiCatalogo - Fundamentos/ApiCatalogo/Logging/CustomLoggerProvider.cs
+++ ApiCatalogo - Fundamentos/ApiCatalogo/Logging/CustomLoggerProvider.cs
@@ -1,6 +1,29 @@
-﻿namespace ApiCatalogo.Logging
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Concurrent;
+
+namespace ApiCatalogo.Logging
 {
-    public class CustomLoggerProvider
+    public class CustomLoggerProvider : ILoggerProvider
     {
+        readonly CustomLoggerProviderConfiguration loggerConfig;
+
+        readonly ConcurrentDictionary<string, CustomerLogger> loggers =
+                 new ConcurrentDictionary<string, CustomerLogger>();
+
+        public CustomLoggerProvider(CustomLoggerProviderConfiguration config)
+        {
+            loggerConfig = config;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return loggers.GetOrAdd(categoryName, name => new CustomerLogger(name, loggerConfig));
+        }
+
+        public void Dispose()
+        {
+            loggers.Clear();
+        }
     }
 }
