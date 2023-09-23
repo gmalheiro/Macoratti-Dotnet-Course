3f68e37c02d48ea94c3d058ccc914daf792c826b
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-09-23 19:10:03 -0300

    Project set

--- ApiCatalogoRepositorio/APICatalogo/APICatalogo/Models/Produto.cs
+++ ApiCatalogoRepositorio/APICatalogo/APICatalogo/Models/Produto.cs
@@ -1,10 +1,9 @@
-﻿using ApiCatalogoRepositorio.Validations;
-using System;
-using System.Collections.Generic;
+﻿using APICatalogo.Validations;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
-namespace ApiCatalogoRepositorio.Models
+namespace APICatalogo.Models
 {
     [Table("Produtos")]
     public class Produto : IValidatableObject
@@ -12,18 +11,19 @@ namespace ApiCatalogoRepositorio.Models
         [Key]
         public int ProdutoId { get; set; }
 
-        [Required(ErrorMessage ="O nome é obrigatório")]
+        [Required(ErrorMessage = "O nome é obrigatório")]
         [StringLength(80, ErrorMessage = "O nome deve ter no máximo {1} e no mínimo {2} caracteres",
-            MinimumLength = 5)]
-        //[PrimeiraLetraMaiuscula]
+           MinimumLength = 5)]
+        [PrimeiraLetraMaiuscula]
         public string? Nome { get; set; }
 
         [Required]
-        [StringLength(300, ErrorMessage = "A descrição deve ter no máximo {1} caracteres")]
+        [StringLength(300)]
         public string? Descricao { get; set; }
 
         [Required]
         [Range(1, 10000, ErrorMessage = "O preço deve estar entre {1} e {2}")]
+        [Column(TypeName = "decimal(10,2)")]
         public decimal Preco { get; set; }
 
         [Required]
@@ -32,9 +32,10 @@ namespace ApiCatalogoRepositorio.Models
 
         public float Estoque { get; set; }
         public DateTime DataCadastro { get; set; }
-
-        public Categoria? Categoria { get; set; }
         public int CategoriaId { get; set; }
+
+        [JsonIgnore]
+        public Categoria? Categoria { get; set; }
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
@@ -51,7 +52,7 @@ namespace ApiCatalogoRepositorio.Models
                 }
             }
 
-            if(this.Estoque <= 0)
+            if (this.Estoque <= 0)
             {
                 yield return new
                        ValidationResult("O estoque deve ser maior que zero",
