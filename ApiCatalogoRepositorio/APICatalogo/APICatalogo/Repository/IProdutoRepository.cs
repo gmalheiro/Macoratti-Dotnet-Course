15084f0c945db15db0e1a7619afe12593906d8c3
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-09-24 02:43:32 -0300

    Created ProdutoRepository

---
+++ ApiCatalogoRepositorio/APICatalogo/APICatalogo/Repository/IProdutoRepository.cs
@@ -0,0 +1,8 @@
+﻿using APICatalogo.Models;
+
+namespace APICatalogo.Repository;
+
+public interface IProdutoRepository : IProdutoRepository<Produto> 
+{
+    IEnumerable<Produto> GetProdutoPorPreco();
+}
---
+++ ApiCatalogoRepositorio/APICatalogo/APICatalogo/Repository/ProdutoRepository.cs
@@ -0,0 +1,16 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+
+namespace APICatalogo.Repository;
+
+public class ProdutoRepository : Repository<Produto> , IProdutoRepository
+{
+    public ProdutoRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public IEnumerable<Produto> GetProdutoPorPreco()
+    {
+        return Get().OrderBy(c => c.Preco).ToList();
+    }
+}
