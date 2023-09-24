8d8c32be95f28c1d0a7f892511ec45c92c0ec8ae
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-09-24 17:49:22 -0300

    Created CategoriaRepository

---
+++ ApiCatalogoRepositorio/APICatalogo/APICatalogo/Repository/CategoriaRepository.cs
@@ -0,0 +1,17 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Repository
+{
+    public class CategoriaRepository : Repository<Categoria>, ICategoriaRepository
+    {
+        private readonly AppDbContext _context;
+
+        public IEnumerable<Categoria> GetCategoriasProduto(AppDbContext context)
+        {
+            AppDbContext _context = context;
+            return Get().Include(x => x.Produtos);
+        }
+    }
+}
